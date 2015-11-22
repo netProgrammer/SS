c0818fb0bd1eddc01a155571779a798454149b80
netProgrammer <vova@programmer.net>
2015-11-22 19:10:33 +0200

    Register, Resolve

--- Funq/Funq.Test/ContainerFixture.cs
+++ Funq/Funq.Test/ContainerFixture.cs
@@ -6,13 +6,39 @@ namespace Funq.Test
     [TestClass]
     public class ContainerFixture
     {
-        [TestMethod]
         public void CreateInstances()
         {
             var bar = new Bar();
             var foo = new Foo(bar);
 
             //var foo2 = Activator.CreateInstance(typeof (Foo)); too expencive
+            Func<Bar> barFactory = () => new Bar();
+            Func<Foo> fooFactory = () => new Foo(barFactory.Invoke());
+            var bar2 = barFactory.Invoke();
+        }
+
+        [TestMethod]
+        public void RegisterTypeAndGetInstance()
+        {
+            var container = new Container();
+            container.Register<IBar>(c => new Bar());
+            var bar = container.Resolve<IBar>();
+            Assert.IsNotNull(bar);
+            Assert.IsTrue(bar is Bar);
+        }
+
+        [TestMethod]
+        public void RegisterTypeAndInjectDependencies()
+        {
+            var container = new Container();
+            container.Register<IBar>(c => new Bar());
+            container.Register<IFoo>(c => new Foo(container.Resolve<IBar>()));
+
+            var foo = container.Resolve<IFoo>();
+
+            Assert.IsNotNull(foo);
+            Assert.IsTrue(foo is Foo);
+
         }
 
         public interface IBar
---
+++ Funq/Funq/Container.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Funq
+{
+    public class Container
+    {
+        private Dictionary<Type, object> _factories = new Dictionary<Type, object>();
+
+        public void Register<TService>(Func<Container, TService> factory)
+        {
+            _factories.Add(typeof (TService), factory);
+        }
+
+        public TService Resolve<TService>()
+        {
+            var factory = _factories[typeof (TService)];
+            return ((Func<Container, TService>) factory).Invoke(this);
+        }
+    }
+}
