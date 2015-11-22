11e021935d2f7f58441f945c5b55b9dd56ef6785
netProgrammer <vova@programmer.net>
2015-11-22 18:33:07 +0200

    init

---
+++ Funq/Funq.Test/ContainerFixture.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Funq.Test
+{
+    [TestClass]
+    public class ContainerFixture
+    {
+        [TestMethod]
+        public void CreateInstances()
+        {
+            var bar = new Bar();
+            var foo = new Foo(bar);
+
+            //var foo2 = Activator.CreateInstance(typeof (Foo)); too expencive
+        }
+
+        public interface IBar
+        {
+             
+        }
+
+        public interface IFoo
+        {
+             
+        }
+
+        public class Bar: IBar
+        {
+             
+        }
+
+        public class Foo: IFoo
+        {
+             public IBar Bar { get; set; }
+
+            public Foo(IBar bar)
+            {
+                Bar = bar;
+            }
+        }
+    }
+}
